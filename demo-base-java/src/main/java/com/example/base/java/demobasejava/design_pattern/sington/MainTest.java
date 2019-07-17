557c70723121f9d7b8a3b8b2078d90c2261edecd
benjaminGzq <gzqytu@163.com>
2019-07-17 18:05:59 +0800

    设计模式： 单例模式， 枚举实现

---
+++ demo-base-java/src/main/java/com/example/base/java/demobasejava/design_pattern/sington/MainTest.java
@@ -0,0 +1,14 @@
+package com.example.base.java.demobasejava.design_pattern.sington;
+
+/**
+ * @author benjamin
+ * created at 2019/7/17
+ */
+public class MainTest {
+    public static void main(String[] args) {
+
+        Singleton.INSTANCE.whateverMethod();
+
+
+    }
+}
---
+++ demo-base-java/src/main/java/com/example/base/java/demobasejava/design_pattern/sington/Singleton.java
@@ -0,0 +1,14 @@
+package com.example.base.java.demobasejava.design_pattern.sington;
+
+/**
+ * @author benjamin
+ * created at 2019/7/17
+ */
+public enum Singleton {
+    INSTANCE;
+
+    public void whateverMethod() {
+    }
+
+
+}
