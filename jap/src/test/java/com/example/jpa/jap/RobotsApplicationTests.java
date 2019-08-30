a68baa5706c7ec07b9219502246812d398805a53
benjaminGzq <gzqytu@163.com>
2019-08-30 16:46:51 +0800

    spring-jdbc数据源的问题

---
+++ jap/src/test/java/com/example/jpa/jap/RobotsApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.jpa.jap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+
+/**
+ * @author benjamin
+ * created at 2019/8/30
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RobotsApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void test() {
+        System.out.println(dataSource.getClass());
+    }
+
+}
+
