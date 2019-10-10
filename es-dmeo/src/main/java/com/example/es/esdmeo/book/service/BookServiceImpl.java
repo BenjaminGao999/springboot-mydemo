5df15b6c7a693a2aaa67e45ec87e2d5debe69ea6
benjaminGzq <gzqytu@163.com>
2019-10-10 18:02:45 +0800

    elasticsearch

---
+++ es-dmeo/src/main/java/com/example/es/esdmeo/book/service/BookServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.es.esdmeo.book.service;
+
+import com.example.es.esdmeo.book.model.Book;
+import com.example.es.esdmeo.book.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void delete(Book book) {
+        bookRepository.delete(book);
+    }
+
+    public Book findOne(String id) {
+        Optional<Book> optional = bookRepository.findById(id);
+        return optional.orElse(null);
+    }
+
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    public Page<Book> findByAuthor(String author, PageRequest pageRequest) {
+        return bookRepository.findByAuthor(author, pageRequest);
+    }
+
+    public List<Book> findByTitle(String title) {
+        return bookRepository.findByTitle(title);
+    }
+
+}
