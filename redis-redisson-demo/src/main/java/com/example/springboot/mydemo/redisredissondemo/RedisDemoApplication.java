c371565244d1f1a00b14d33bea68514437b84e18
benjaminGzq <gzqytu@163.com>
2019-10-28 09:16:30 +0800

    cache框架 redis作为cache provider @EnableCaching

--- redis-redisson-demo/src/main/java/com/example/springboot/mydemo/redisredissondemo/RedisDemoApplication.java
+++ redis-redisson-demo/src/main/java/com/example/springboot/mydemo/redisredissondemo/RedisDemoApplication.java
@@ -3,9 +3,9 @@ package com.example.springboot.mydemo.redisredissondemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
 public class RedisDemoApplication {
 
     public static void main(String[] args) {
