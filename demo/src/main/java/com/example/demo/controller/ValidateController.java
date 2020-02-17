f827092ec33ba376b3e6dcb13b6de3bcdfee15fd
gaozhiqiang <gzqytu@163.com>
2020-02-17 08:55:57 +0800

    beanutil

--- demo/src/main/java/com/example/demo/controller/ValidateController.java
+++ demo/src/main/java/com/example/demo/controller/ValidateController.java
@@ -2,9 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.ValidatedPOJO;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author benjamin
@@ -17,5 +22,14 @@ public class ValidateController {
     public String validatedTest(@Validated @RequestBody ValidatedPOJO pojo) {
         return pojo.toString();
 
+    }
+
+
+    @DeleteMapping("/delete/ids")
+    public String batchDelete(@RequestBody @NotEmpty List<Integer> ids) {
+        String x = Arrays.toString(ids.toArray());
+        System.out.println(x);
+
+        return x;
     }
 }
