7d712e9b73123e295f148539f894657552bfd3c6
benjaminGzq <gzqytu@163.com>
2020-09-01 14:55:17 +0800

    swagger2 测试

--- demo/src/main/java/com/example/demo/config/Swagger2Config.java
+++ demo/src/main/java/com/example/demo/config/Swagger2Config.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -51,7 +52,7 @@ public class Swagger2Config {
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
                 .description("更多请关注http://www.baidu.com")
                 .termsOfServiceUrl("http://www.baidu.com")
-                .contact("sunf")
+                .contact(new Contact("联系人", "url", "email"))
                 .version("1.0")
                 .build();
     }
